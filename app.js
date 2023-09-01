5c9261351582026762c3f80399281267bd69ca69
KelvinMutuku <mutukuk553@gmail.com>
2023-09-01 12:08:20 +0300

    Responsiveness added to big & small tablets

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
 
-hamburger.addEventListener("click", () => {
-hamburger.classList.toggle("change");
-navLinks.classList.toggle("nav-change");
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
 });
